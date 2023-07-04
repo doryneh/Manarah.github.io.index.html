eeea44023f32bad2402eb9d71f908d9d9487f814
Dory Nehme <doryy.nehme@gmail.com>
2023-07-05 01:25:07 +0300

    finishing the landing page , still have to fix the header and the responsiveness

--- js/js.js
+++ js/js.js
@@ -100,5 +100,26 @@ $(document).ready(function() {
 
   // Set the default background color
   rangeInput.trigger('input');
+
+
+
+
 });
 
+
+$(document).ready(function() {
+  $('.accordion-button').on('click', function() {
+    var $button = $(this);
+    var $icon = $button.find('svg');
+
+    if ($button.hasClass('collapsed')) {
+      // Button is collapsed, change to expanded state
+      $icon.removeClass('fa-minus').addClass('fa-plus');
+
+    } else {
+      // Button is expanded, change to collapsed state
+      $icon.removeClass('fa-plus').addClass('fa-minus');
+
+    }
+  });
+});
